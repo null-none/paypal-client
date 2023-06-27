e7ae3a154637289ce975f9b0d11d7536a0666344
Dmitry Kalinin <kalinin.mitko@gmail.com>
2023-06-27 19:57:22 +0300

    Added CI/CD

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="paypal_client",
     version="0.2.1",
     packages=find_packages(),
+    description="The PayPal API provides simple client for RESTful APIs",
+    long_description="Simple client for PayPal RESTful APIs",
     author="Dmitry Kalinin",
     url="https://github.com/null-none/paypal-client",
     install_requires=[
